2140716d0131910b78aec0f889d42a6277d4ece2
Elfetnimourad <moradtao2000@gmail.com>
2025-07-29 12:02:05 +0100

    gh-pages

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,14 @@
 "use strict";
+/**
+ * Main E-commerce product and cart functionality
+ *
+ * Features:
+ * - Fetches and displays products
+ * - Responsive mobile menu
+ * - Add to cart functionality
+ * - Cart item quantity management
+ * - Dynamic total price calculation
+ */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,7 +18,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// Get the container where product cards will be displayed
+// DOM elements setup
 let show = document.querySelector('.show');
 let header = document.querySelector('.header');
 let sideBarMoreIcon = document.querySelector(".sidebar-more-icon");
@@ -21,44 +31,52 @@ let cartShop = document.querySelector('.fa-cart-shopping');
 let cartBadge = document.querySelector('.cart-with-badge');
 let beforeCart = window.getComputedStyle(cartShop, '::after');
 let closeIcon = document.querySelector(".fa-xmark");
-const badge = document.createElement('div');
 let arrOfItemsList = document.querySelector(".arr-of-items");
 let sidebar = document.querySelector('.sidebar');
-let totPrice = document.createElement("span");
 let logout = document.querySelector('.logout');
+// Badge element for cart counter
+const badge = document.createElement('div');
+badge.classList.add("badge");
+// Total price element
+let totPrice = document.createElement("span");
 totPrice.classList.add("totPrice");
+// Cart and quantity tracking
 let count = 0;
 let countedItem = 1;
 let quantity = { quantity: 0 };
 let t = 0;
-let arrOfItems = [];
+// Set to track added items for duplication check
 let tryArr = new Set();
+// Product arrays
+let arrOfItems = [];
 let ab = [];
+// Sidebar show/hide
 cartShop === null || cartShop === void 0 ? void 0 : cartShop.addEventListener('click', () => {
     sidebar.classList.remove('d-none');
 });
 closeIcon === null || closeIcon === void 0 ? void 0 : closeIcon.addEventListener('click', () => {
     sidebar.classList.add('d-none');
 });
-// Menu Icon 
+// Responsive menu toggle
 menu.addEventListener('click', () => {
     sideBarMoreIcon.classList.remove('d-none');
 });
 closeMoreIcon.addEventListener('click', () => {
     sideBarMoreIcon.classList.add('d-none');
 });
-// Async function to fetch and display products
+// Set home link color
 home.style.color = "black";
+/**
+ * Fetch and display product data
+ */
 function getApi() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            // Fetch product data from DummyJSON API
             const res = yield fetch("https://dummyjson.com/products");
             const data = yield res.json();
-            // Log full data to the console
-            console.log(data);
-            // Filter out specific products by title
+            // Filter out unwanted products
             let arr = data.products.filter((e) => e.title !== 'Beef Steak' && e.title !== 'Cucumber');
+            // Responsive mobile layout setup
             if (window.innerWidth < 800) {
                 let iconsPart = document.createElement('div');
                 iconsPart.className = 'icons-wrapper';
@@ -89,35 +107,33 @@ function getApi() {
                 });
             }
             let copiedArray = [...arr];
-            // Iterate over the filtered products
+            /**
+             * Render filtered products and enable cart actions
+             * @param copiedArray - list of products to display
+             */
             function getResult(copiedArray) {
                 copiedArray.forEach((element) => {
-                    // Create individual elements for each product
+                    // Create product card elements
                     let heading = document.createElement('h5');
                     let image = document.createElement('img');
                     let card = document.createElement('div');
                     let price = document.createElement('p');
                     let buyBtn = document.createElement('button');
-                    // Set content for elements
                     heading.innerHTML = element.title;
                     image.src = element.images[0];
                     price.innerHTML = `Price: ${element.price}$`;
                     buyBtn.innerText = "BUY";
-                    // Style the image
+                    // Style elements
                     image.style.width = '290px';
                     image.style.height = '220px';
                     image.style.padding = '15px';
-                    // Style the price
                     price.style.paddingBottom = '5px';
                     price.style.paddingRight = '5px';
-                    // Style the Buy button
                     buyBtn.style.width = '99%';
                     buyBtn.style.height = '25px';
                     buyBtn.style.backgroundColor = '#007BFF';
                     buyBtn.style.color = 'white';
                     buyBtn.style.borderRadius = '7px';
-                    buyBtn.style.alignSelf = 'center';
-                    // Style the card
                     card.style.height = '400px';
                     card.style.borderRadius = '10px';
                     card.style.backgroundColor = 'white';
@@ -128,19 +144,17 @@ function getApi() {
                     card.style.margin = '10px';
                     card.style.boxShadow = '0 2px 8px rgba(0, 0, 0, 0.1)';
                     card.style.padding = '10px';
-                    // Append elements to card
+                    // Append elements
                     card.appendChild(image);
                     card.appendChild(heading);
                     card.appendChild(price);
                     card.appendChild(buyBtn);
-                    // Append card to the main container
                     show.appendChild(card);
-                    //Add To Cart
-                    badge.classList.add('badge');
-                    cartBadge.appendChild(badge);
                     let toot;
                     let objItem = Object.assign(Object.assign({}, element), { quantity, toot });
-                    // Full Details for the Each Product 
+                    /**
+                     * Show full product details when clicking image
+                     */
                     image.addEventListener('click', () => {
                         show.innerHTML = '';
                         let partImage = document.createElement("div");
@@ -151,7 +165,7 @@ function getApi() {
                         let descrptionProduct = document.createElement("p");
                         let productPrice = document.createElement('span');
                         let productDimensions = document.createElement("h5");
-                        let productStock = document.createElement("h5");
+                        let productStock = document.createElement("p");
                         for (let i = 0.1; i <= element.rating; i++) {
                             let rateProduct = document.createElement("i");
                             rateProduct.classList = 'fa-solid fa-star';
@@ -161,13 +175,13 @@ function getApi() {
                         }
                         show.style.height = '80vh';
                         productImage.src = element.images[0];
+                        productImage.className = 'productImage';
                         productImage.style.border = '1px solid lightblue';
                         productImage.style.boxShadow = '2px 2px 2px lightblue';
                         productTitle.innerText = 'Title :' + element.title;
                         descrptionProduct.innerText = "Product Description :" + element.description;
                         productPrice.innerText = 'Price :' + element.price + "$";
-                        productDimensions.innerHTML = "Dimensions :" + JSON.stringify((element.dimensions));
-                        productImage.style.height = '50vh';
+                        productDimensions.innerHTML = "Dimensions :" + JSON.stringify(element.dimensions);
                         productStock.innerHTML = "Stock :" + element.stock;
                         partImage.appendChild(productImage);
                         partDetails.appendChild(productTitle);
@@ -175,6 +189,7 @@ function getApi() {
                         partDetails.appendChild(productDimensions);
                         partDetails.appendChild(productPrice);
                         partDetails.appendChild(productStock);
+                        partDetails.className = 'partDetails';
                         productCard.style.display = 'flex';
                         productCard.style.flexDirection = 'row';
                         productCard.style.width = '150vh';
@@ -183,21 +198,17 @@ function getApi() {
                         productCard.appendChild(partDetails);
                         show.appendChild(productCard);
                     });
-                    //Search Bar Input 
+                    /**
+                     * Handle add to cart action
+                     */
                     buyBtn.addEventListener('click', () => {
-                        console.log('let us see before', arrOfItems);
-                        console.log(quantity.quantity++, 'rr');
-                        badge.classList.add('badge');
                         count++;
-                        console.log(count);
                         badge.innerHTML = count.toString();
                         cartBadge.style.transform = 'translateY(10%)';
                         objItem.quantity++;
                         objItem.toot = (objItem.quantity * objItem.price) || objItem.price;
                         arrOfItems.push(objItem);
                         arrOfItemsList.innerHTML = "";
-                        let arrOfSetItemsFilter = arrOfItems.filter((e, i) => i === arrOfItems.indexOf(e));
-                        console.log('filtred array', arrOfSetItemsFilter);
                         let arrOfSetItems = new Set([...arrOfItems]);
                         let ar = [];
                         arrOfSetItems.forEach((ele, index) => {
@@ -208,31 +219,23 @@ function getApi() {
                                 let amount = document.createElement("span");
                                 let decBtn = document.createElement("button");
                                 let incBtn = document.createElement("button");
+                                // Increment
                                 incBtn.addEventListener("click", () => {
                                     ele.quantity++;
                                     amount.innerHTML = "Amount :" + ele.quantity;
                                     ele.toot = ele.quantity * ele.price;
-                                    ar.push(totNumber);
                                     count++;
                                     badge.innerHTML = count.toString();
-                                    console.log('ar', ar);
-                                    amount.appendChild(incBtn);
-                                    amount.appendChild(decBtn);
-                                    console.log("ele of increment cart shop", ele);
                                     let sum = [...arrOfSetItems.values()].map((e) => e.toot).reduce((e, c) => e + c);
                                     totPrice.innerHTML = 'Total Price :' + sum.toString() + "$";
                                 });
+                                // Decrement
                                 decBtn.addEventListener("click", () => {
                                     ele.quantity--;
                                     amount.innerHTML = "Amount :" + ele.quantity;
                                     ele.toot = ele.quantity * ele.price;
-                                    ar.push(totNumber);
-                                    console.log('ar', ar);
                                     count--;
                                     badge.innerHTML = count.toString();
-                                    amount.appendChild(incBtn);
-                                    amount.appendChild(decBtn);
-                                    console.log("ele of decrement cart shop", ele);
                                     let sum = [...arrOfSetItems.values()].map((e) => e.toot).reduce((e, c) => e + c);
                                     totPrice.innerHTML = 'Total Price :' + sum.toString() + "$";
                                     if (count === 0) {
@@ -244,56 +247,48 @@ function getApi() {
                                         arrOfSetItems.delete(ele);
                                     }
                                 });
-                                if (ele.quantity === 0) {
-                                    cardItem.remove();
-                                    arrOfSetItems.delete(ele);
-                                }
-                                console.log("eleeeeeeee", ele);
-                                incBtn.innerHTML = '+';
-                                decBtn.innerHTML = '-';
-                                // let totalPrice = arrOfItems.reduce((e,c)=>e.price + c.price);
-                                cardItem.style.width = '100%';
-                                imgItem.style.height = '80px';
-                                imgItem.style.width = '80px';
-                                imgItem.src = ele.images[0];
-                                titleItem.innerHTML = ele.title;
-                                console.log('ele', ele);
+                                // Ensure quantity initialized
                                 if (!tryArr.has(ele)) {
                                     ele.quantity = 1;
                                     tryArr.add(ele);
                                 }
                                 amount.innerHTML = "Amount :" + ele.quantity;
-                                console.log('true, it is the same');
                                 let totNumber = ele.quantity * ele.price;
-                                console.log("totNumber", totNumber);
                                 ar.push(totNumber);
-                                console.log(ar, "ar");
+                                incBtn.innerHTML = '+';
+                                decBtn.innerHTML = '-';
+                                imgItem.src = ele.images[0];
+                                imgItem.style.height = '80px';
+                                imgItem.style.width = '80px';
+                                titleItem.innerHTML = ele.title;
                                 amount.appendChild(incBtn);
                                 amount.appendChild(decBtn);
                                 cardItem.appendChild(imgItem);
                                 cardItem.appendChild(titleItem);
                                 cardItem.appendChild(amount);
                                 arrOfItemsList.appendChild(cardItem);
-                                console.log(ele.toot);
                             }
                         });
+                        // Append total price
                         let sum = [...arrOfSetItems.values()].map((e) => e.toot).reduce((e, c) => e + c);
                         totPrice.innerHTML = 'Total Price :' + sum.toString() + "$";
                         arrOfItemsList.appendChild(totPrice);
-                        console.log('sum', sum);
                         arrOfItemsList.style.overflow = "scroll";
                     });
                 });
             }
+            // If no search term, show all
             if (searchBar.value === '') {
                 getResult(copiedArray);
             }
+            // Search functionality
             searchBar.addEventListener('change', () => {
                 if (searchBar.value !== '' && copiedArray.some(el => el.title === searchBar.value)) {
                     show.innerHTML = '';
                     getResult(copiedArray.filter(el => el.title === searchBar.value));
                 }
             });
+            // Remove badge if cart is empty
             if (count === 0) {
                 badge.classList.remove('badge');
             }
@@ -303,5 +298,5 @@ function getApi() {
         }
     });
 }
-// Call the function to start loading data
+// Start the app
 getApi();
