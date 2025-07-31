53fa0340e96d986466be3a41fa342b45eb5fd56b
Elfetnimourad <moradtao2000@gmail.com>
2025-07-31 02:01:07 +0100

    Updates

--- pages/signup.js
+++
@@ -1,5 +0,0 @@
-// import { loginFun } from "./logout.js";
-// let signUpBtn = document.querySelector("button");
-// let email = document.getElementsByName("email");
-// let password = document.getElementsByName("password");
-// loginFun(signUpBtn, email, password);
